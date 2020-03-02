927339eff30ceab9f7f4f5cb733aba3025ecc04b
0vqv4a3 <0vqmuhammad@gmail.com>
2020-03-02 21:04:10 +0800

    set tick , start and pause method for the timer, and use arrow function instead a shorthand for making method inside class Timer because '' this'' will not refer to instance of class if we use shorthand. arrow function cannot be extended to other class because it didn't enter the__ proto__ what is running behind the scene of arrow func is defineProperty() method of object

--- Timer/index.js
+++ Timer/index.js
@@ -1,28 +1,35 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton) {
+    ///the code inside constructor function will
+    ///immediately running in instance a new object created
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    // eventlistener is added here so that in instance an object created to make sure when start button clicked
+    //the start method will be called this is called binding
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
 
-        this.startButton.addEventListener('click', this.start)
-    }
 
-    //method for timer to start
-    start() {
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
 
-    }
-    //
-    pause() {
+  pause = () => {
+    clearInterval(this.interval);
+  };
 
-    }
-    onDurationChange() {
-
-    }
-    tick() {
-
-    }
+  tick = () => {
+    let timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = --timeRemaining;
+  };
 }
 
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
