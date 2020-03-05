8bc460b0dcee3b17b345b29fa17c84c63e92a1f5
0vqv4a3 <0vqmuhammad@gmail.com>
2020-03-05 16:19:43 +0800

    fetching  mpvie data for moviefight project from omdbapi

--- moviesFight/index.js
+++ moviesFight/index.js
@@ -1,1 +1,14 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '8c14c8ce',
+            s: 'avengers'
+
+        }
+
+    });
+    console.log(response.data);
+    console.log(response);
+};
+
+fetchData();
