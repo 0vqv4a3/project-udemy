47805959e2eaf123446e3a733ba393e1f2b419b6
0vqv4a3 <0vqmuhammad@gmail.com>
2020-03-13 22:16:46 +0800

    refactoring the code through creating an object inside index.js, the object will have fetch property for fetching data, and renderOption property its a function that know how to render something like movie, and optionSelect property its a function that handle when user click an option and root property inside it the autocomplete will be rendered

--- moviesFight/index.js
+++ moviesFight/index.js
@@ -15,57 +15,6 @@ const fetchData = async searchTerm => {
     return response.data.Search;
 };
 
-//create html element for reusable dropdown widget
-// all the classname is based on bulma css framework for styling the dropdown
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input"/>
-<div class="dropdown">
-<div class="dropdown-menu">
-<div class="dropdown-content results"></div>
-</div>
-</div>
-`;
-
-// select the input tag and other necessary optionMovies
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultWrapper = document.querySelector(".results");
-
-// asign the callback to fetch data and pass it to event listener
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-
-    // checking if movies containt something and remove 'is-active' for hide the dropdown
-    if (!movies.length) {
-        dropdown.classList.remove("is-active");
-        return;
-    }
-    //make resultWrapper empty string so the search from before didn't stack
-    resultWrapper.innerHTML = "";
-    dropdown.classList.add("is-active");
-
-    for (let movie of movies) {
-        const optionMovies = document.createElement("a");
-        const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-        optionMovies.classList.add("dropdown-item");
-        optionMovies.innerHTML = `
-            <img src="${imgSrc}"/>
-            ${movie.Title}
-        `;
-        /// add an eventListener for passing the movie name that user select
-        /// and pass the movie name to input.value so it show up in input it's like autocomplete movie name,
-        /// and close the dropdown menu and run function for follow up request and the rendering in html
-        optionMovies.addEventListener('click', (event) => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-
-        resultWrapper.appendChild(optionMovies);
-    }
-};
 
 // add function for handling follow up request after user select a movie
 const onMovieSelect = async (movie) => {
@@ -117,13 +66,3 @@ const movieTemplate = (movieDetail) => {
     </article>
     `;
 }
-
-// the second arg in debounce for the delay
-input.addEventListener("input", debounce(onInput, 500));
-
-// add a click event for closing the dropdown if user is clicking outside root element and it's child
-document.addEventListener("click", event => {
-    if (!root.contains(event.target)) {
-        dropdown.classList.remove("is-active");
-    }
-});
