9c0bf96ddcc4c1b75963b70257b70f98c8d1dc9b
0vqv4a3 <0vqmuhammad@gmail.com>
2020-03-29 13:15:15 +0800

    make ls command line system like linux, for reading all files inside directory

--- List/index.js
+++ List/index.js
@@ -1,1 +1,12 @@
-console.log("there");
+/* make ls command line like linux based CLI that read the file in user directory filesystem with nodejs.org library */
+// import fs(filesystem module) from Nodejs.org/api it's standard library for node.js
+const fs = require('fs');
+
+// run readdir() in fs object for reading all file inside a directory
+fs.readdir('..', (err, filesname) => {
+
+    if (err) {
+        console.log(err);
+    }
+    console.log(filesname);
+});
