14c3ff53ac4eff3616ca80dce7effa0a4917aeb6
0vqv4a3 <0vqmuhammad@gmail.com>
2020-03-29 14:45:39 +0800

    make package.json from CLI (npm init -y) it will create a new file package.json its describe some different qualities of our project

--- List/index.js
+++ List/index.js
@@ -2,8 +2,9 @@
 // import fs(filesystem module) from Nodejs.org/api it's standard library for node.js
 const fs = require('fs');
 
-// run readdir() in fs object for reading all file inside a directory
-fs.readdir('..', (err, filesname) => {
+// run readdir() in fs object for reading all file inside a directory, Note: process.pwd() Current Working Directory(cwd)
+// the same as saying search current working directory from where exactly this program(index.js) excecuted from. 
+fs.readdir(process.cwd(), (err, filesname) => {
 
     if (err) {
         console.log(err);
