9ac7d027f06b82f25cbfdecd221fe194188a9a89
0vqv4a3 <0vqmuhammad@gmail.com>
2020-04-01 08:01:01 +0800

    small refactoring inside async getAll() funct

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -16,21 +16,23 @@ class UsersRepository {
 
   async getAll() {
     // Open the file called this.filename
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: "utf8"
-    });
     // Read it's content
-    console.log(contents);
     // Parse the content
-
     // Return the parsed data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8"
+      })
+    );
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  await repo.getAll();
+  const users = await repo.getAll();
+
+  console.log(users);
 };
 
 test();
