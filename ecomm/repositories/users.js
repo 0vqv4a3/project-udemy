56cc699a06e3ad88415f5db7c7fdf8e108f38822
0vqv4a3 <0vqmuhammad@gmail.com>
2020-04-01 08:29:05 +0800

    make async create() function for writing and update data from sign Up and record it back to this.filename(json file)

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -25,8 +25,17 @@ class UsersRepository {
       })
     );
   }
-}
 
+  // create an account with the data from user Sign Up, by writing the data inside this.filename(json file) so it will be recorded in server
+  // attrs(attribute) the data that being returned from user sign Up like {email: dars@gmail.com, password:'a1233as'}
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs); // records is an array of object
+
+    // write the updated 'records' back to this.filename
+    fs.promises.writeFile(this.filename, JSON.stringify(records)); //JSON.stringify()convert js value to Javascript Object Notation(JSON) string
+  }
+}
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
