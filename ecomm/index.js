b88f9b295c7fcb685bbf59453f29c8e5ae982bc9
0vqv4a3 <0vqmuhammad@gmail.com>
2020-04-09 10:32:35 +0800

    adding cookie-session library , npm install cookie-session. for handling cookie and encrypted it with key property with array of random string as value so cookie-session library encrypting our user ID for protection purpose, so mallicious person cannot impersonate as other users to access as admin, by pasting this cookie with tool like POSTMAN for request to this Domain, and impersonate as other user if the cookie didn't encrypted

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // parsing inputs value in form to key : value pairs in object
+const cookieSession = require("cookie-session"); // cookie-session library is a middleware function like body-parser so we will use the use() method from express to implement it in all our Route Handling particulary this will add additional property to req object every time the follow up request was made by user for this Domain and will use it as authentication which user is signIn in this site as administrator
 //import instance UserRepository from users.js inside repositories
 const usersRepo = require("./repositories/users");
 
@@ -8,6 +9,11 @@ const app = express();
 // .use() function applied  middleware func to all Route Handler, either is GET or POST or something else
 // in this case bodyParser will parsing all req in every request Handler
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    key: ["32jg54ug5uy4g35gt4o3u2tg328"], // this key property is added so cookieSession library will encrypt the user ID inside the cookie, so malicious people can't pretend as different user to access this site as administrator or user
+  })
+);
 
 //the first arg is anytime someone make request to the route(path) , '/' its the path
 app.get("/", (req, res) => {
@@ -41,7 +47,7 @@ app.post("/", async (req, res) => {
 
   // Create a user inside users repo to represent this person(user) that signUp
   const user = await usersRepo.create({ email, password }); // usersRepo.create() will return attrs(user) object with its unique ID
-  // Store the id of that user inside the user Cookie
+  // Store the id of that user inside the users Cookie
 
   res.send("Account succesly created!!!");
 });
